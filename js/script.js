abfc7cbcf6ebd42afa37c3f811fc217501a17fb2
Soro Cat <sorousha326@gmail.com>
2019-06-14 10:38:23 -0700

    add changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
+
+
+
+
+// user interface logic
 $(document).ready(function() {
   $("form#pig-latin").submit(function(event) {
     event.preventDefault();
-  });
+    var pig = parseInt($("input#pig").val());
+    var result = latin(year);
+  }
 });
