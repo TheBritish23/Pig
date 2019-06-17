fade331f5a0dc315bf641c9878d595c6a847b172
Soro Cat <sorousha326@gmail.com>
2019-06-17 14:02:27 -0700

    add changes

--- js/script.js
+++ js/script.js
@@ -4,12 +4,30 @@ var translation = function(sentence){
   for(x=0; x < newsentence.length; x++){
     pig.forEach(function(letter){
      if(newsentence[x] === letter){
+       newsentence[x]= "-ay";
+     };
+   });
+ }
+console.log(newsentence)
+return newsentence;
+}
 
 
 // user interface logic
 $(document).ready(function() {
   $("form#pig-latin").submit(function(event) {
     event.preventDefault();
+    var pig = parseInt($("input#year").val());
 
 
     var words = $('#words').val()
+
+    var newsentence = translation(words)
+
+    $('#output').append(newsentence)
+
+    $("#display").append(x);
+})
+
+
+  });
